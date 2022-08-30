41bcb8a76d8e07b250e8ffefdf374b00d7569bd2
Paul Perhaita <paul.perhaita@ceff.ch>
2022-08-30 16:20:13 +0200

    Fin des tableaux

--- Bulltin_PHP/bulletin.php
+++ Bulltin_PHP/bulletin.php
@@ -15,26 +15,42 @@
 
 <?php
 // Tableau des modules
-$tab_mod["ICH 106"]["desc"] = "- Interroger, traiter et assurer la maintenance des bases de données"; //Description
+$tab_mod["ICH 106"]["desc"] = " - Interroger, traiter et assurer la maintenance des bases de données"; //Description
 $tab_mod["ICH 106"]["note"] = 3.5; //Note
-$tab_mod["ICH 117"]["desc"] = "- Mettre en place l'infrastructure informatique d'une petite entreprise"; //Description
+$tab_mod["ICH 117"]["desc"] = " - Mettre en place l'infrastructure informatique d'une petite entreprise"; //Description
 $tab_mod["ICH 117"]["note"] = 4.5; //Note
-$tab_mod["ICH 122"]["desc"] = "- Automatiser des procédures à l'aide de scripts et de macros"; //Description
+$tab_mod["ICH 122"]["desc"] = " - Automatiser des procédures à l'aide de scripts et de macros"; //Description
 $tab_mod["ICH 122"]["note"] = 5.0; //Note
-$tab_mod["ICH 162"]["desc"] = "- Analyser et modéliser des données"; //Description
+$tab_mod["ICH 162"]["desc"] = " - Analyser et modéliser des données"; //Description
 $tab_mod["ICH 162"]["note"] = 4.5; //Note
-$tab_mod["ICH 164"]["desc"] = "- Créer des bases de données et y insérer des données"; //Description
+$tab_mod["ICH 164"]["desc"] = " - Créer des bases de données et y insérer des données"; //Description
 $tab_mod["ICH 164"]["note"] = 5.5; //Note
-$tab_mod["ICH 231"]["desc"] = "- Appliquer la protection et la sécurité des données"; //Description
+$tab_mod["ICH 231"]["desc"] = " - Appliquer la protection et la sécurité des données"; //Description
 $tab_mod["ICH 231"]["note"] = 5.0; //Note
-$tab_mod["ICH 319"]["desc"] = "- Concevoir et implémenter des applications"; //Description
+$tab_mod["ICH 319"]["desc"] = " - Concevoir et implémenter des applications"; //Description
 $tab_mod["ICH 319"]["note"] = 4.5; //Note
-$tab_mod["ICH 431"]["desc"] = "- Concevoir et implémenter des applications"; //Description
+$tab_mod["ICH 431"]["desc"] = " - Concevoir et implémenter des applications"; //Description
 $tab_mod["ICH 431"]["note"] = 4.5; //Note
 
 //Tableau des CIE
+$tab_cie["ICH 123"]["desc"] = "- Activer les services d´un serveur"; //Description
+$tab_cie["ICH 123"]["desc"] = 5.0; //Note
+$tab_cie["ICH 187"]["desc"] = "- Mettre en service un poste de travail ICT avec le système d’exploitation"; //Description
+$tab_cie["ICH 187"]["desc"] = 5.0; //Note
+$tab_cie["ICH 216"]["desc"] = "- Intégrer les terminaux IoE dans une plateforme existante"; //Description
+$tab_cie["ICH 216"]["desc"] = 5.5; //Note
+$tab_cie["ICH 216"]["desc"] = "- Créer et publier un site Web"; //Description
+$tab_cie["ICH 216"]["desc"] = 4.5; //Note
 
-
+foreach ($tab_mod AS $key => $value){
+    echo"<table>";
+        echo"<tr>"
+        echo $key;
+    echo $tab_mod[$key]["desc"];
+    echo $tab_mod[$key]["note"];
+   echo "<br>";
+   echo"</table>";
+}
 ?>
 
 </body>
