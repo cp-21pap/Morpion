2c4e11d0569d07f28fff1df481369d65430b07c2
Paul Perhaita <paul.perhaita@ceff.ch>
2022-08-30 15:54:33 +0200

    Tableau Modules

--- Bulltin_PHP/bulletin.php
+++ Bulltin_PHP/bulletin.php
@@ -7,18 +7,34 @@
 </head>
 <body>
 
-
-//Titre de la page
+<!--Titre de la page-->
 <h2>Bulletin ICH</h2>
 
+<!--Sous-titre (Modules)-->
 <h3>Modules de compétences en informatique </h3>
-<?php
-$tab_mod["117"]["informatique ---"] = "Informatique"
 
-foreach ($modules AS $key => $value){
-    echo $key. " --> ".$value;
-    echo "<br>";
-}
+<?php
+// Tableau des modules
+$tab_mod["ICH 106"]["desc"] = "- Interroger, traiter et assurer la maintenance des bases de données"; //Description
+$tab_mod["ICH 106"]["note"] = 3.5; //Note
+$tab_mod["ICH 117"]["desc"] = "- Mettre en place l'infrastructure informatique d'une petite entreprise"; //Description
+$tab_mod["ICH 117"]["note"] = 4.5; //Note
+$tab_mod["ICH 122"]["desc"] = "- Automatiser des procédures à l'aide de scripts et de macros"; //Description
+$tab_mod["ICH 122"]["note"] = 5.0; //Note
+$tab_mod["ICH 162"]["desc"] = "- Analyser et modéliser des données"; //Description
+$tab_mod["ICH 162"]["note"] = 4.5; //Note
+$tab_mod["ICH 164"]["desc"] = "- Créer des bases de données et y insérer des données"; //Description
+$tab_mod["ICH 164"]["note"] = 5.5; //Note
+$tab_mod["ICH 231"]["desc"] = "- Appliquer la protection et la sécurité des données"; //Description
+$tab_mod["ICH 231"]["note"] = 5.0; //Note
+$tab_mod["ICH 319"]["desc"] = "- Concevoir et implémenter des applications"; //Description
+$tab_mod["ICH 319"]["note"] = 4.5; //Note
+$tab_mod["ICH 431"]["desc"] = "- Concevoir et implémenter des applications"; //Description
+$tab_mod["ICH 431"]["note"] = 4.5; //Note
+
+//Tableau des CIE
+
+
 ?>
 
 </body>
