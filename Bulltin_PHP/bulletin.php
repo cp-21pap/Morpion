c6aabd879b5f452fa7c7ec4e2ae00492e0549168
Paul Perhaita <paul.perhaita@ceff.ch>
2022-09-02 08:35:00 +0200

    Try to make an average

--- Bulltin_PHP/bulletin.php
+++ Bulltin_PHP/bulletin.php
@@ -34,24 +34,54 @@ $tab_mod["ICH 431"]["note"] = 4.5; //Note
 
 //Tableau des CIE
 $tab_cie["ICH 123"]["desc"] = "- Activer les services d´un serveur"; //Description
-$tab_cie["ICH 123"]["desc"] = 5.0; //Note
+$tab_cie["ICH 123"]["note"] = 5.0; //Note
 $tab_cie["ICH 187"]["desc"] = "- Mettre en service un poste de travail ICT avec le système d’exploitation"; //Description
-$tab_cie["ICH 187"]["desc"] = 5.0; //Note
+$tab_cie["ICH 187"]["note"] = 5.0; //Note
 $tab_cie["ICH 216"]["desc"] = "- Intégrer les terminaux IoE dans une plateforme existante"; //Description
-$tab_cie["ICH 216"]["desc"] = 5.5; //Note
+$tab_cie["ICH 216"]["note"] = 5.5; //Note
 $tab_cie["ICH 216"]["desc"] = "- Créer et publier un site Web"; //Description
-$tab_cie["ICH 216"]["desc"] = 4.5; //Note
+$tab_cie["ICH 216"]["note"] = 4.5; //Note
 
-foreach ($tab_mod AS $key => $value){
+//Boucle d'affichage des Modules
+foreach ($tab_mod AS $id_mod => $value){
     echo"<table>";
-        echo"<tr>"
-        echo $key;
-    echo $tab_mod[$key]["desc"];
-    echo $tab_mod[$key]["note"];
-   echo "<br>";
+        echo"<tr>";
+            echo $id_mod; //Affichage des IDs des CCO
+            echo $tab_mod[$id_mod]["desc"]; //Affichage des description des CCO
+            echo $tab_mod[$id_mod]["note"]; //Affichage des notes des CCO
+            echo "<br>";
+        echo"</tr>";
    echo"</table>";
 }
+   $somme_mod = 0;
+   $i = 0;
+    foreach ($tab_mod[$id_mod]["note"] as $key => $valeur) {
+        $somme_mod += $valeur;
+        $i++;
+   }
+$average = $somme_mod / $i;
+
 ?>
+
+<!--Sous-titre (CIE)-->
+<h3>Cours Interentreprise</h3>
+
+<?php
+//Boucle d'affichage des CIE
+foreach ($tab_cie AS $id_cie => $value) {
+    echo"<table>";
+        echo"<tr>";
+            echo $id_cie; // Affichage des IDs des CIE
+            echo $tab_cie[$id_cie]["desc"]; //Affichage des description des CIE
+            echo $tab_cie[$id_cie]["note"]; //Affichage des notes des CIE
+        echo "</tr>";
+    echo"</table>";
+}
+?>
+
+<!--Sous-titre (CIE)-->
+<h3>Compétences en informatique</h3>
+
 
 </body>
 </html>
