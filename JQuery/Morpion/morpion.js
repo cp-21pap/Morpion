d02fee1e249c1c60b1fe72651a7924bbc9c83d05
Paul Perhaita <paul.perhaita@ceff.ch>
2022-09-27 09:46:57 +0200

    Morpion 1.1
    It's working in principe

--- JQuery/Morpion/morpion.js
+++ JQuery/Morpion/morpion.js
@@ -5,45 +5,43 @@ $(function (){
    $("td").click(function (){
 
        if(!$(this).hasClass("symbole_0") && !$(this).hasClass("symbole_1")){
-           $(this).addClass(("symbole_") + ((turn) ? "0" : "1"))
+           $(this).addClass(("symbole_") + ((turn) ? "1" : "0"))
            turn =! turn // Inverser la valeur de bool
        }
        move++;
-       if(findWinner() != -1 && findWinner() != ""){
-           if(findWinner() == ("symbole_1")){
-               $('body').append('<div class="winner"><pan>Winner</pan>X</div>');
-               $('body').append('<button onclick="location.reload()">Play Again</button>');
-               $('.winner').css('background-color', '#61892f');
-               $('button').css('color', '#61892f');
-           }else {
-               $('body').append('<div class="winner"><pan>Winner</pan>O</div>');
-               $('body').append('<button onclick="location.reload()">Play Again</button>');
-               $('.winner').css('background-color', '#61892f');
-               $('button').css('color', '#61892f');
-           }
-           turn = false;
-       }
+            if (findWinner() == ("symbole_1")) {
+                $('body').append('<div class="winner"><span>Winner</span> X</div>');
+                $('body').append('<button onclick="location.reload()">Play Again</button>');
+                $('.winner').css('background-color', '#61892f');
+                $('button').css('color', '#61892f');
+            } else if (findWinner() == ("symbole_0")) {
+                $('body').append('<div class="winner"><pan>Winner</pan> O</div>');
+                $('body').append('<button onclick="location.reload()">Play Again</button>');
+                $('.winner').css('background-color', '#ff0000');
+                $('button').css('color', '#ff0000');
+            }
    })
 });
 
 function findWinner() {
-    cs1 = $("table tr:nth-child(1) td:nth-child(1)").text();
-    cs2 = $("table tr:nth-child(1) td:nth-child(2)").text();
-    cs3 = $("table tr:nth-child(1) td:nth-child(3)").text();
-    cs4 = $("table tr:nth-child(2) td:nth-child(1)").text();
-    cs5 = $("table tr:nth-child(2) td:nth-child(2)").text();
-    cs6 = $("table tr:nth-child(2) td:nth-child(3)").text();
-    cs7 = $("table tr:nth-child(3) td:nth-child(1)").text();
-    cs8 = $("table tr:nth-child(3) td:nth-child(2)").text();
-    cs9 = $("table tr:nth-child(3) td:nth-child(3)").text();
+    cs1 = $("table tr:nth-child(1) td:nth-child(1)").attr('class');
+    cs2 = $("table tr:nth-child(1) td:nth-child(2)").attr('class');
+    cs3 = $("table tr:nth-child(1) td:nth-child(3)").attr('class');
+    cs4 = $("table tr:nth-child(2) td:nth-child(1)").attr('class');
+    cs5 = $("table tr:nth-child(2) td:nth-child(2)").attr('class');
+    cs6 = $("table tr:nth-child(2) td:nth-child(3)").attr('class');
+    cs7 = $("table tr:nth-child(3) td:nth-child(1)").attr('class');
+    cs8 = $("table tr:nth-child(3) td:nth-child(2)").attr('class');
+    cs9 = $("table tr:nth-child(3) td:nth-child(3)").attr('class');
 
     //check rows
     if((cs1 == cs2) && (cs2 == cs3)){
+        console.log(cs3);
         return cs3;
     } else if ((cs4 == cs5) && (cs5 == cs6)){
         return cs6;
     }else if ((cs7 == cs8) && (cs8 == cs9)){
-    return cs9;
+        return cs9;
     }
 
     //check columns
